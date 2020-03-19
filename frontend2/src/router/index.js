e482fb0c4a349bb56f9b24ee38bec12f2cf3534f
Buddy Chambers <blueshift@intelligent-d2.com>
2020-03-19 01:42:32 -0500

    Updated Vue client, and frontend modules

---
+++ frontend2/src/router/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Articles from '@/components/Articles'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Articles',
+      component: Articles
+    }
+  ]
+})
